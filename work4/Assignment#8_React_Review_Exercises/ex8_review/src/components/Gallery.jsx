99061b9e8c4465e2e0d947ef6dc2445d2509461a
tongcns <tongandtom55@gmail.com>
2023-03-23 12:25:35 +0700

    again

---
+++ work4/Assignment#8_React_Review_Exercises/ex8_review/src/components/Gallery.jsx
@@ -0,0 +1,12 @@
+function Gallery() {
+    return (
+    <section>
+        <h2>"Gallery"</h2>
+        <ul>
+
+            <li><a href= "https://chat.openai.com/chat">Site2</a></li>
+            <li><a href= "https://chat.openai.com/chat">Site3</a></li>
+        </ul>
+    </section>
+    )
+}
