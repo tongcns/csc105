b4299fc1c5f13abfc39f9b0bcda7c573218d778b
tongcns <tongandtom55@gmail.com>
2023-03-26 10:54:04 +0700

    new

--- Assignment9/assign9/src/components/List.jsx
+++ Assignment9/assign9/src/components/List.jsx
@@ -2,15 +2,15 @@ import Card from "./Card";
 
 function List() {
     const person = [ 
-    {Name:"John Doe", job:"Engineer", hobby: "Listen to music"},
-    {Name:"John Doe", job:"Engineer", hobby: "Listen to music"},
-    {Name:"John Smith", job:"Programmer", hobby: "Running"},
-    {Name:"John Smith", job:"Programmer", hobby: "Running"},
+    {name:"John Doe", job:"Engineer", hobby: "Listen to music"},
+    {name:"John Doe", job:"Engineer", hobby: "Listen to music"},
+    {name:"John Smith", job:"Programmer", hobby: "Running"},
+    {name:"John Smith", job:"Programmer", hobby: "Running"},
 ];
     return (
        <>
        {person.map((e) => {
-        return <Card name={e.Name} job={e.job} hobby={e.hobby}></Card>
+        return <Card name={e.name} job={e.job} hobby={e.hobby}></Card>
        }
        )}
        </>
