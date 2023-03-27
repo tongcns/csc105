18533c933acbe7b20e74b66d8fa08690e5ceb1e2
tongcns <tongandtom55@gmail.com>
2023-03-27 14:55:22 +0700

    new

---
+++ Lab3/todo_app3_1/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Todo from "./components/Todo";
+// import Home from "./components/Home";
+// import Todo from "./components/Todo";
+import TodoPage from "./pages/TodoPage";
+function App() {
+    return (
+        // <div className="App">
+        //     <div className="home">
+        //     <Home />
+        // </div>
+        // <div className="todo">
+        //     <Todo />
+        // </div>
+        // </div>
+        <TodoPage />
+    );
+}
+
+export default App;
---
+++ Lab3/todo_app3_1/src/components/BottomNav.jsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import RestoreIcon from "@mui/icons-material/Restore";
+import { CalendarMonth, Today } from "@mui/icons-material";
+
+export default function BottomNav() {
+  const [value, setValue] = React.useState("");
+  // const navigate = navigate();
+  const handleChange = (event, newValue) => {
+    // setValue(newValue);
+  };
+
+  return (
+    <BottomNavigation
+      sx={{ width: "100%" }}
+      value={value}
+      onChange={handleChange}
+    >
+      <BottomNavigationAction
+        label="Incoming"
+        value="todo"
+        icon={<RestoreIcon />}
+      />
+      <BottomNavigationAction label="Today" value="today" icon={<Today />} />
+      <BottomNavigationAction
+        label="Calendar"
+        value="calendar"
+        icon={<CalendarMonth />}
+      />
+    </BottomNavigation>
+
+    
+  );
+}
