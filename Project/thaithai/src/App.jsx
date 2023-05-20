55edee35fc8bfc23d3959f89e7c8b83ba651941c
realchanatong <88615326+tongcns@users.noreply.github.com>
2023-05-20 17:49:00 +0700

    d ja

---
+++ Project/thaithai/src/App.jsx
@@ -0,0 +1,21 @@
+import { Box } from '@mui/material';
+import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+import Sidebar from './components/Sidebar';
+import './App.css';
+
+function App() {
+  return (
+    <box>
+      {/* <Navbar /> */}
+      {/* <Login /> */}
+      {/* <Signup /> */}
+      <Home />
+      {/* <Sidebar /> */}
+    </box>
+  );
+}
+
+export default App;
